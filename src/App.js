d94f0b295312f824ca690bc438ca247b7b605e90
xixilide <754396775@qq.com>
2016-11-28 09:21:51 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component, PropTypes} from 'react'
+import $ from 'jquery'
+// import { Navbar, Nav, NavItem } from 'react-bootstrap'; // 导航组件
+
+export default class App extends Component {
+  render(){
+    return(
+      <div>
+          <p>个人简介</p>
+      </div>
+    )
+  }
+}
